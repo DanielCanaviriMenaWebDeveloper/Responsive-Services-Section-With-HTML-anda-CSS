84d425dfa852e778cf5cdcf977ff6b5b44c5c880
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2022-11-11 23:55:49 -0400

    Sidebar Navigation Menu Finalizado

---
+++ Sidebar Navigation Menu/app.js
@@ -0,0 +1,22 @@
+/* Funcionalidad para el menu hamburguesa */
+let menuToggle = document.querySelector('.menuToggle');
+let navigation = document.querySelector('.navigation');
+
+menuToggle.onclick = function() {
+    navigation.classList.toggle('active');
+}
+
+/* Funcionalidad para Link Activo */
+let list = document.querySelectorAll('.list');
+
+function activeLink() {
+    list.forEach( (item) => 
+        item.classList.remove('active')
+    );
+    this.classList.add('active');
+}
+
+list.forEach( (item) => 
+    item.addEventListener('click', activeLink) 
+);
+
